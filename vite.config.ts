aa96b9ec187b5fe13c82ad2368f1acc7d271e2ef
Brady Pierce <bradypierce@Bradys-MacBook-Pro.local>
2025-06-08 16:15:35 -0700

    Add shadcn/tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -7,5 +9,11 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
